dade44ee3dc148e518c480a8d0d2ce8ac4212c17
huilinghe19 <huiling.he@helmholtz-berlin.de>
2018-10-18 10:27:18 +0200

    3 hello_world functions, just with self.output can work in spock console

--- macros_huiling/macros_new/salute.py
+++ macros_huiling/macros_new/salute.py
@@ -3,3 +3,17 @@ from sardana.macroserver.macro import Macro, macro, Type
 @macro()
 def hello_world(self):
     self.output('Running hello_world...')
+
+
+@macro()
+def hello_world2(self):
+    """This is an hello world macro"""
+    print("Hello, World!")
+
+    
+class HelloWorld(Macro):
+    """Hello, World! macro"""
+
+    def run(self):
+        print "Hello, World!"
+
