6750a8a455a2ca8e6f2e42c905be9347ab7f2255
huilinghe19 <huiling.he@helmholtz-berlin.de>
2019-01-09 10:28:08 +0100

    the controller works well

--- controllers_practices/copleyController.py
+++ controllers_practices/copleyController.py
@@ -6,13 +6,14 @@ from sardana.pool.controller import DefaultValue, Description, FGet, FSet, Type
 
 class SerialObject(object):
     def __init__(self, port):
-        self.ser = serial.Serial(port, 9600, timeout=0.5) 
-   
-    def readPosition(self):  
-        ser= self.ser 
+        self.ser = serial.Serial(port, 9600, timeout=0.5)
+
+    def readPosition1(self):
+        ser= self.ser
         result =""
+        ser.write("g r0xa0\n")
         while True:
-            data =ser.read(1) 
+            data =ser.read(1)
             print "Read::", data
             if not data:
                 break
@@ -25,12 +26,32 @@ class SerialObject(object):
         position = result[2:]
         print position
         return float(position)
-    
-    def readStateVariable(self):  
-        ser= self.ser 
+
+    def readPosition2(self):
+        ser= self.ser
         result =""
+        ser.write("2 g r0xa0\n")
         while True:
-            data =ser.read(1) 
+            data =ser.read(1)
+            print "Read::", data
+            if not data:
+                break
+            if data =="\n":
+                break
+            if data == "\r":
+                continue
+            result += data
+        print result
+        position = result[2:]
+        print position
+        return float(position)
+
+    def readStateVariable1(self):
+        ser= self.ser
+        result =""
+        ser.write("g r0x24\n")
+        while True:
+            data =ser.read(1)
             print "Read::", data
             if not data:
                 break
@@ -43,83 +64,177 @@ class SerialObject(object):
         state_id = result[2:4]
         print state_id
         return int(state_id)
-         
+
+    def readStateVariable2(self):
+        ser= self.ser
+        result =""
+        ser.write("2 g r0x24\n")
+        while True:
+            data =ser.read(1)
+            print "Read::", data
+            if not data:
+                break
+            if data =="\n":
+                break
+            if data == "\r":
+                continue
+            result += data
+        print result
+        state_id = result[2:4]
+        print state_id
+        return int(state_id)
+
+    def setMotorPosition1(self, position):
+        ser= self.ser
+        result =""
+        ser.write("s r0xca {}\n".format(position))
+
+        while True:
+            data =ser.read(1)
+            print "Read::", data
+            if not data:
+                break
+            if data =="\n":
+                break
+            if data == "\r":
+                continue
+            result += data
+        print result
+        return result
+
+    def setMotorPosition2(self, position):
+        ser= self.ser
+        result =""
+        ser.write("2 s r0xca {}\n".format(position))
+
+        while True:
+            data =ser.read(1)
+            print "Read::", data
+            if not data:
+                break
+            if data =="\n":
+                break
+            if data == "\r":
+                continue
+            result += data
+        print result
+        return result
+
+    def moveMotor2(self):
+        ser= self.ser
+        result =""
+        ser.write("2 t 1\n")
+        while True:
+            data =ser.read(1)
+            print "Read::", data
+            if not data:
+                break
+            if data =="\n":
+                break
+            if data == "\r":
+                continue
+            result += data
+        print result
+        return result
+
+    def moveMotor1(self):
+        ser= self.ser
+        result =""
+        ser.write("t 1\n")
+        while True:
+            data =ser.read(1)
+            print "Read::", data
+            if not data:
+                break
+            if data =="\n":
+                break
+            if data == "\r":
+                continue
+            result += data
+        print result
+        return result
+
     def write(self,data):
         ser=self.ser
-        print "Write::", data 
+        print "Write::", data
         ser.write(data)
-        
+
     def writePosition(self,position):
         ser=self.ser
         print "Write motor Position::", position
         ser.write("t 1\n")
-        
+
     def writePosition2(self,position):
         ser=self.ser
-        print "Write motor2 Position::", position 
+        print "Write motor2 Position::", position
         ser.write("2 t 1\n")
-        
-    def read(self,n):  
-        ser= self.ser         
-        data =ser.read(n) 
+
+    def read(self,n):
+        ser= self.ser
+        data =ser.read(n)
         print "Read::", data
-        return data 
+        return data
+
 class CopleyController(MotorController):
-   
+
     ctrl_properties = \
         {
          "Port": {Type : str,
                   Description : "Serial Port",
-                  DefaultValue : '/dev/ttyS0'},
+                  DefaultValue : "/dev/ttyS0"},
         }
     AXIS_NAMES = {1: "stepnet01", 2: "stepnet02"}
 
     STATES = {"ON": State.On, "OFF": State.Off}
-  
+
     def __init__(self, inst, props, *args, **kwargs):
         MotorController.__init__(self,inst, props, *args, **kwargs)
-        #port = self.Port
         self.copleyController = SerialObject("/dev/ttyS0")
-        
+
     def __del__(self):
         del self.copleyController
-        
+
     def StateOne(self, axis):
         axis_name = self.AXIS_NAMES[axis]
         copleyController = self.copleyController
-        copleyController.write("g r0x24\n") 
-    	result = copleyController.readStateVariable()
+        if axis_name == "stepnet01":
+            result = copleyController.readStateVariable1()
+        elif axis_name == "stepnet02":
+            result = copleyController.readStateVariable2()
         print result
-    	if result:           
+        if result:
             state = self.STATES["ON"]
-        else: 
-           state = self.STATES["OFF"]
+        else:
+            state = self.STATES["OFF"]
         limit_switches = MotorController.NoLimitSwitch
-        #print state, limit_switches
         return state, limit_switches
-    
+
     def ReadOne(self, axis):
         axis_name = self.AXIS_NAMES[axis]
         copleyController = self.copleyController
-        if axis_name == "stepnet01":            
-            copleyController.write("g r0xA0\n")     
+        if axis_name == "stepnet01":
+            position = copleyController.readPosition1()
         elif axis_name =="stepnet02":
-            copleyController.write("2 g r0xA0\n")        
-        position = copleyController.readPosition()
+            position = copleyController.readPosition2()
         return position
-    
-    def StartOne(self, axis, position):   
+
+    def StartOne(self, axis, position):
         axis_name = self.AXIS_NAMES[axis]
         copleyController = self.copleyController
-        if axis_name =="stepnet02":
-	    copleyController.write("2 s r0xca {}\n".format(position))
-            copleyController.write("2 t 1\n")
-	    sleep(1)            
-        elif axis_name =="stepnet01":
-	    ans = copleyController.write("s r0xca {}\n t 1\n".format(position))
-	    sleep(1)
+        if axis_name =="stepnet01":
+            a = copleyController.setMotorPosition1(position)
+            if a:
+                ans = copleyController.moveMotor1()
+            else:
+                print a
+
+        elif axis_name =="stepnet02":
+            a = copleyController.setMotorPosition2(position)
+            if a:
+                ans = copleyController.moveMotor2()
+            else:
+                print a
 
     def AbortOne(self, axis):
         copleyController = self.copleyController
         copleyController.write("r\n 2 r\n")
-
